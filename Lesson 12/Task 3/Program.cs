57433a59600a08597bc180c21ae2c2d9b8f64fe0
IbrahimliCavid <ibrahimli.dev@gmail.com>
2023-12-05 21:37:56 +0400

    upload

---
+++ Lesson 12/Task 3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        const string FolderDirectory = "C:\\Test qovluq";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FilesAndFoldersCount());
+        }
+
+        static int FilesAndFoldersCount()
+        {
+            
+
+
+            string directoryName = FolderDirectory;
+
+                var folderInfo = new DirectoryInfo(directoryName);
+                var files = folderInfo.GetFiles("*", SearchOption.AllDirectories);
+                var folders = folderInfo.GetDirectories("*", SearchOption.AllDirectories);
+          
+               return files.Length + folders.Length;
+            
+
+           
+        }
+    }
+}
