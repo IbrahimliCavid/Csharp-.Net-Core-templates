7da8bd500f1f215c5c60a2d7720c6d50c3b5390f
IbrahimliCavid <ibrahimli.dev@gmail.com>
2023-12-18 21:54:17 +0400

    upload

--- lesson 13/Task 1/Program.cs
+++ lesson 13/Task 1/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetDriveInfo();
+        }
+
+        static void GetDriveInfo()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in allDrives)
+            {
+
+                if (drive.IsReady)
+                {
+                    var totalSize = ByteToMb(drive.TotalSize);
+                    var availableSize = ByteToMb(drive.AvailableFreeSpace);
+                    var usedSize = totalSize - availableSize;
+                    Console.WriteLine( $"{drive.Name} \n" +
+                        $"Total size: {totalSize} GB \n" +
+                        $"Avilabe size: {availableSize} Gb \n" +
+                        $"Used size: {usedSize} GB");
+                }
+                else
+                {
+                    Console.WriteLine(drive.Name + "is not ready.");
+                }
+              
+            }
+        }
+
+        static long ByteToMb(long bytes)
+        {
+            return  bytes / (1024 * 1024 * 1024);
         }
     }
 }
